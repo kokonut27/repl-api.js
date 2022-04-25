e79b62de568354bc1de537787629f69dcf6dad01
kokonut <beol0127@gmail.com>
2022-04-25 22:27:34 +0000

    Initial commit

---
+++ repl-api/index.js
@@ -0,0 +1,25 @@
+// repl-api package in nodejs
+import fs from 'fs';
+import path from 'path';
+import fetch from 'node-fetch';
+import constants from './utils/constants.js';
+
+let user = "JBloves27";
+
+const response = await fetch(constants.graphql, {
+	method: 'post',
+  headers: constants.headers,
+	body: JSON.stringify({
+		query: `
+query User($user: String!) {
+  userByUsername(username: $user) {
+    karma
+  }
+}`,
+		variables: JSON.stringify({
+			user
+		})
+	})
+}).then((res) => res.json());
+
+const data = await response.json();
---
+++ repl-api/utils/constants.js
@@ -0,0 +1,11 @@
+export default {
+  graphql: "https://replit.com/graphql",
+  headers: {
+    'Content-Type': 'application/json',
+  	Accept: 'application/json',
+  	Connection: 'keep-alive',
+  	'X-Requested-With': 'the ReplAPI.it Project',
+  	Referrer: 'https://replit.com/',
+  	Origin: 'https://replit.com/'
+  }
+}
