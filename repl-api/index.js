aec263df0ec88edbee15d63b8099680f9e076d7c
kokonut <beol0127@gmail.com>
2022-04-26 20:43:47 +0000

    chore: update async function

--- repl-api/index.js
+++ repl-api/index.js
@@ -6,23 +6,26 @@ import constants from './utils/constants.js';
 
 let user = "JBloves27";
 
-const response = await fetch(constants.graphql, {
-	method: 'post',
-  headers: constants.headers,
+const info = await fetch(constants.graphql, {
+	method: 'POST',
+	headers: constants.headers,
 	body: JSON.stringify({
-		query: `
-query User($user: String!) {
-  userByUsername(username: $user) {
-    karma
-  }
-}`,
-		variables: JSON.stringify({
-			user
-		})
-	})
+  	query: `
+      query User($user: String!) {
+        userByUsername(username: $user) {
+          karma
+        }
+      }`,
+  	variables: JSON.stringify({
+  		user,
+  	}),
+  }),
 }).then((res) => res.json());
 
-const data = await response.json();
+// const data = await response.();
+(async () => {
+  console.log(response);
+})();
 
 export default {
   // add the main piece function here
