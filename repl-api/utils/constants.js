8421bbec71aeeacf56218404f4d573fd0f564e61
kokonut <beol0127@gmail.com>
2022-05-02 01:22:27 +0000

    chore: update headers

--- repl-api/utils/constants.js
+++ repl-api/utils/constants.js
@@ -6,7 +6,7 @@ export default {
     'Content-Type': 'application/json',
   	Accept: 'application/json',
   	Connection: 'keep-alive',
-  	'X-Requested-With': 'the ReplAPI.it Project',
+  	'X-Requested-With': 'JBloves27',
   	Referrer: 'https://replit.com/',
   	Origin: 'https://replit.com/'
   }
