785b7267f02ec3968e95731688272dd0c190a104
kokonut <beol0127@gmail.com>
2022-05-05 01:16:07 +0000

    feat: update json formating!

--- repl-api/user.sh
+++ repl-api/user.sh
@@ -1,1 +1,5 @@
-curl -X POST https://replit.com/graphql -H "Content-Type: application/json" -d '{"query":"query User($username: String!) {\n  userByUsername(username: $username) {\n    fullName\n    bio\n    karma\n  }\n}\n","variables":{"username":"JBloves27"}}' -H "X-Requested-With: JBloves27" -H "Referrer: https://replit.com"
+curl -X POST https://replit.com/graphql -H "Content-Type: application/json" -d '{"query":"query User($username: String!) {\n  userByUsername(username: $username) {\n    fullName\n    bio\n    karma\n  }\n}\n","variables":{"username":"JBloves27"}}' -H "X-Requested-With: JBloves27" -H "Referrer: https://replit.com" > .pretty-replapi.json
+
+clear
+
+jq . .pretty-replapi.json
