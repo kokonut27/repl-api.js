479c17d06252d60e8115fb2d9387830fbecbcf20
kokonut <beol0127@gmail.com>
2022-05-05 01:25:00 +0000

    chore: add comment for later

--- repl-api/user.sh
+++ repl-api/user.sh
@@ -1,4 +1,4 @@
-curl -X POST https://replit.com/graphql -H "Content-Type: application/json" -d '{"query":"query User($username: String!) {\n  userByUsername(username: $username) {\n    fullName\n    bio\n    karma\n  }\n}\n","variables":{"username":"JBloves27"}}' -H "X-Requested-With: repl-api.js" -H "Referrer: https://replit.com" > .pretty-replapi.json
+curl -X POST https://replit.com/graphql -H "Content-Type: application/json" -d '{"query":"query User($username: String!) {\n  userByUsername(username: $username) {\n    fullName\n    bio\n    karma\n  }\n}\n","variables":{"username":"JBloves27"}}' -H "X-Requested-With: repl-api.js" -H "Referrer: https://replit.com" > .pretty-replapi.json # update username here.
 
 clear
 
