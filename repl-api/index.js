1a9c6d2420d55eea8ca7ed3ad9c30a7e69c25867
kokonut <beol0127@gmail.com>
2022-05-09 17:24:38 +0000

    feat: move classes.js directory

--- repl-api/index.js
+++ repl-api/index.js
@@ -4,7 +4,7 @@ import path from 'path';
 import User from './classes/user.js';
 import Repls from './classes/repls.js';
 import Talk from './classes/talk.js';
-import classes from './classes.js';
+import classes from './utils/classes.js';
 
 const replapi = new User("JBloves27");
 
---
+++ repl-api/utils/classes.js
@@ -0,0 +1,9 @@
+import User from '../classes/user.js';
+import Repls from '../classes/repls.js';
+import Talk from '../classes/talk.js';
+
+export default {
+  user: User,
+  repls: Repls,
+  talk: Talk
+}
