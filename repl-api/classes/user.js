1a86dfd6b747fa4e38c701ffe4bab531dc168a36
kokonut <beol0127@gmail.com>
2022-05-12 16:04:48 +0000

    feat: add errors and add comment

--- repl-api/classes/user.js
+++ repl-api/classes/user.js
@@ -4,9 +4,14 @@ import constants from '../utils/constants.js';
 import getData from "../utils/request.js"
 
 export default class User {
+  /**
+  Constructs the Replit username
+  @param username Enter the Replit username
+  */
   constructor(username) {
     this.username = username;
   }
+  
   /**
   Get all data on a Replit user
   @param shouldSimplify Simplify the data returned from replit
@@ -17,11 +22,19 @@ export default class User {
     let data = await getData(constants.queries.userFull, { 
       "username": username,
     });
-    if (shouldSimplify) {
-      data = data.data.userByUsername;
-      data.url = "https://replit.com" + data.url;
+    if (data.errors) {
+      throw new Error(`The Replit GraphQL Server returned an error: ${data.errors}`);
     }
-    return data;
+    if (!data.data.userByUsername) {
+      throw new Error(`The Replit username '${username}' does not exist`);
+    } else {
+      if (shouldSimplify) {
+        data = data.data.userByUsername;
+        data.url = "https://replit.com" + data.url;
+      }
+  
+      return data;
+    }
   }
 
   /**
@@ -30,18 +43,28 @@ export default class User {
   */
   async userCompressed(shouldSimplify) {
     const username = this.username;
-    // const shouldSimplify = this.shouldSimplify;
 
     let data = await getData(constants.queries.user, { 
       "username": username,
     });
-    if (shouldSimplify) {
-      data = data.data.userByUsername;
-      data.url = "https://replit.com" + data.url;
+    if (data.errors) {
+      throw new Error(`The Replit GraphQL Server returned an error: ${data.errors}`);
     }
-    return data;
+    if (!data.data.userByUsername) {
+      throw new Error(`The Replit username '${username}' does not exist`);
+    } else {
+      if (shouldSimplify) {
+        data = data.data.userByUsername;
+        data.url = "https://replit.com" + data.url;
+      }
+      return data;
+    }
   }
 
+  /**
+  Get post data on a Replit user
+  @param shouldSimplify Simplify the data returned from replit
+  */
   async userPosts(shouldSimplify, count=10, order="new") {
     const username = this.username;
 
@@ -51,13 +74,24 @@ export default class User {
       "order": order,
       "after": ""
     });
-    if (shouldSimplify) {
-      data = data.data.userByUsername;
-      data.url = "https://replit.com" + data.url;
+    if (data.errors) {
+      throw new Error(`The Replit GraphQL Server returned an error: ${data.errors}`);
     }
-    return data.errors;
+    if (!data.data.userByUsername) {
+      throw new Error(`The Replit username '${username}' does not exist`);
+    } else {
+      if (shouldSimplify) {
+        data = data.data.userByUsername;
+        data.url = "https://replit.com" + data.url;
+      }
+      return data;
+    }
   }
 
+  /**
+  Get comment data on a Replit user
+  @param shouldSimplify Simplify the data returned from replit
+  */
   async userComments(shouldSimplify, count=10, order="new") {
     const username = this.username;
 
@@ -67,10 +101,17 @@ export default class User {
       "order": order,
       "after": ""
     });
-    if (shouldSimplify) {
-      data = data.data.userByUsername;
-      data.url = "https://replit.com" + data.url;
+    if (data.errors) {
+      throw new Error(`The Replit GraphQL Server returned an error: ${data.errors}`);
     }
-    return data;
+    if (!data.data.userByUsername) {
+      throw new Error(`The Replit username '${username}' does not exist`);
+    } else {
+      if (shouldSimplify) {
+        data = data.data.userByUsername;
+        data.url = "https://replit.com" + data.url;
+      }
+      return data;
+    }
   }
 }
